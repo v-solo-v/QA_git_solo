4d61db402afd5f388e48f481e9a5554c0ab36952
v-solo-v <126344210+v-solo-v@users.noreply.github.com>
2023-02-25 23:33:30 +0300

    Update Main.java

--- src/main/java/ru/makler63/Main.java
+++ src/main/java/ru/makler63/Main.java
@@ -2,7 +2,7 @@ package ru.makler63;
 
 public class Main {
     
-    private static String msg = "Hello ladies!";
+    private static String msg = "Hello ladies and gentelmen!";
     
     public static void main(String[] args) {
         System.out.println(msg);
