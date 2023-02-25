459232922df19fbf97ba09b68846b6abf54c1587
v-solo-v <126344210+v-solo-v@users.noreply.github.com>
2023-02-25 23:05:49 +0300

    "Hello ladies!"

--- src/main/java/ru/makler63/Main.java
+++ src/main/java/ru/makler63/Main.java
@@ -2,6 +2,6 @@ package ru.makler63;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello people!");
+        System.out.println("Hello ladies!");
     }
 }
