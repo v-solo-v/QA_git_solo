cd1b2641d0998962bbc13c85809108376106f8ec
v-solo-v <126344210+v-solo-v@users.noreply.github.com>
2023-02-25 23:14:50 +0300

    Update Main.java

--- src/main/java/ru/makler63/Main.java
+++ src/main/java/ru/makler63/Main.java
@@ -1,7 +1,10 @@
 package ru.makler63;
 
 public class Main {
+    
+    private static String msg = "Hello ladies!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello ladies!");
+        System.out.println(msg);
     }
 }
