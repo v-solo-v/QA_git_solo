ef35414495c617c579b15fb7551a77cc6c910f92
Solo VV <makler63@yandex.ru>
2023-02-25 23:47:53 +0300

    rename msg_new

--- src/main/java/ru/makler63/Main.java
+++ src/main/java/ru/makler63/Main.java
@@ -2,9 +2,9 @@ package ru.makler63;
 
 public class Main {
     
-    private static String msg = "Hello ladies and gentelmen!";
+    private static String msg_new = "Hello ladies and gentelmen WWWWWWW!";
     
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(msg_new);
     }
 }
